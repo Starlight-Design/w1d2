f03f56310e1dcecd8fd622a0b8ab0a8f4f00a117
Thomas Yoon <thomas081891@hotmail.com>
2014-10-02 19:53:53 +0000

    new

---
+++ calculator/calculator.rb
@@ -0,0 +1,15 @@
+puts "This program adds two numbers for you"
+
+puts "Enter the first number: "
+
+first_number = gets.chomp.to_i
+
+puts "Enter the second number: "
+
+second_number = gets.chomp.to_i
+
+answer_num = first_number + second_number
+
+puts answer_num
+
+                  
